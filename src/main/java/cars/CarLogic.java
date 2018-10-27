64d31e08639acc831f5e3e5efe5c3914be57dec3
Vitalii A <adviva.work@gmail.com>
2018-10-28 01:03:35 +0300

    CarLogic filter methods optimization

--- src/main/java/cars/CarLogic.java
+++ src/main/java/cars/CarLogic.java
@@ -1,5 +1,6 @@
 package cars;
 
+import java.util.Arrays;
 import java.util.Calendar;
 
 public class CarLogic {
@@ -13,45 +14,36 @@ public class CarLogic {
 
     public static Car[] filterByBrand(Car[] cars, Brand brand) {
         Car[] dest = new Car[cars.length];
-        Car[] res;
         int i = 0;
         for (Car car : cars) {
             if (car != null && car.getBrand() == brand) {
                 dest[i++] = car;
             }
         }
-        res = new Car[i];
-        if (i > 0) System.arraycopy(dest, 0, res, 0, i);
-        return res;
+        return Arrays.copyOf(dest, i);
     }
 
     public static Car[] filterByModelAge(Car[] cars, String model, int age) {
-        final int currYear = Calendar.getInstance().get(Calendar.YEAR);
+        final int edgeYear = Calendar.getInstance().get(Calendar.YEAR) - age;
         Car[] dest = new Car[cars.length];
-        Car[] res;
         int i = 0;
         for (Car car : cars) {
             if (car != null && car.getModel().equalsIgnoreCase(model)
-                    && (currYear - car.getYear()) > age) {
+                    && (car.getYear()) < edgeYear) {
                 dest[i++] = car;
             }
         }
-        res = new Car[i];
-        if (i > 0) System.arraycopy(dest, 0, res, 0, i);
-        return res;
+        return Arrays.copyOf(dest, i);
     }
 
     public static Car[] filterByYearPrice(Car[] cars, int year, double price) {
         Car[] dest = new Car[cars.length];
-        Car[] res;
         int i = 0;
         for (Car car : cars) {
             if (car != null && car.getYear() == year && car.getPrice() > price) {
                 dest[i++] = car;
             }
         }
-        res = new Car[i];
-        if (i > 0) System.arraycopy(dest, 0, res, 0, i);
-        return res;
+        return Arrays.copyOf(dest, i);
     }
 }
